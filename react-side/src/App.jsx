021818b2282998fa7f47fa32d6ffefc5ae96a5a9
AdomasKazlauskas <118741626+AdomasKazlauskas@users.noreply.github.com>
2023-04-03 17:11:04 +0300

    basic-structure

---
+++ react-side/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <header>header</header>
+      <section>hero</section>
+      <section>how it works</section>
+      <section>add a new story</section>
+      <section>story list</section>
+      <section>learn more</section>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
