91546a1372459c673762fa3a43c5b4110412d7a5
AdomasKazlauskas <118741626+AdomasKazlauskas@users.noreply.github.com>
2023-04-03 22:01:55 +0300

    HowItWorks-static

--- react-side/src/App.jsx
+++ react-side/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
-      <section>how it works</section>
+      <HowItWorks />
       <section>add a new story</section>
       <section>story list</section>
       <section>learn more</section>
