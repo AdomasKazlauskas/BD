5d70dda19d2c0bc56a3c7b59d1eaa30d1d234411
AdomasKazlauskas <118741626+AdomasKazlauskas@users.noreply.github.com>
2023-04-10 21:11:21 +0300

    before-preparing

--- react-side/src/components/FundraiseFor.jsx
+++ react-side/src/components/FundraiseFor.jsx
@@ -4,9 +4,12 @@ import hands from "../assets/hands.png";
 import charity from "../assets/charity.png";
 import { useNavigate } from "react-router-dom";
 import { paths } from "../constants/routing";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 function FundraiseFor() {
   const navigate = useNavigate();
+  const { authName } = useContext(GlobalContext);
 
   return (
     <>
@@ -17,7 +20,7 @@ function FundraiseFor() {
         </div>
         <button
           className="fundraise-for-button"
-          onClick={() => navigate(paths.ADDSTORY)}
+          onClick={() => navigate(authName ? paths.ADDSTORY : paths.SIGNUP)}
         >
           <img className="fundraiser-icon" src={hand} alt="hand" />
           <h2>Yourself</h2>
@@ -25,7 +28,7 @@ function FundraiseFor() {
         </button>
         <button
           className="fundraise-for-button"
-          onClick={() => navigate(paths.ADDSTORY)}
+          onClick={() => navigate(authName ? paths.ADDSTORY : paths.SIGNUP)}
         >
           <img className="fundraiser-icon" src={hands} alt="hands" />
           <h2>Community</h2>
@@ -33,7 +36,7 @@ function FundraiseFor() {
         </button>
         <button
           className="fundraise-for-button"
-          onClick={() => navigate(paths.ADDSTORY)}
+          onClick={() => navigate(authName ? paths.ADDSTORY : paths.SIGNUP)}
         >
           <img className="fundraiser-icon" src={charity} alt="charity" />
           <h2>Charity</h2>
